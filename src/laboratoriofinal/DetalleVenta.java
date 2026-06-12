1b1cb136daf400d97556b0f212012fa573d1973b
Danilopro233 <1028leandro@gmail.com>
2026-06-12 14:13:18 -0600

    Compartiendo clases

--- src/laboratoriofinal/DetalleVenta.java
+++ src/laboratoriofinal/DetalleVenta.java
@@ -1,5 +1,18 @@
-package laboratoriofinal;
 
 public class DetalleVenta {
+    private int idDetalle, cantidad;
+    private double subtotal;
+    private Producto producto;
 
+    public DetalleVenta(int id, int cant, Producto prod) {
+        this.idDetalle = id;
+        this.cantidad = cant;
+        this.producto = prod;
+        this.subtotal = calcularSubtotal();
+    }
+
+    public double calcularSubtotal() {
+        return this.cantidad * producto.getPrecioVenta();
+    }
+    // Getters y Setters [cite: 108-112]
 }
---
+++ src/laboratoriofinal/Venta.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Venta {
+    private int idVenta;
+    private String fecha;
+    private double total;
+    private Cliente cliente;
+    private Empleado empleado;
+    private ArrayList<DetalleVenta> detalles;
+
+    public Venta(int id, String fecha, Cliente cli, Empleado emp) {
+        this.idVenta = id;
+        this.fecha = fecha;
+        this.cliente = cli;
+        this.empleado = emp;
+        this.detalles = new ArrayList<>();
+    }
+
+    public void agregarDetalle(DetalleVenta det) {
+        detalles.add(det);
+    }
+
+    public double calcularTotal() {
+        double suma = 0;
+        for (DetalleVenta d : detalles) {
+            suma += d.getSubtotal();
+        }
+        return suma;
+    }
+    // Getters y Setters [cite: 68-77]
+}
